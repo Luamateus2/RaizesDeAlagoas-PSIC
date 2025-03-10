5e787651285bee1f5e394c09fa7698d84a89fac1
Rafaela Urtiga <rafaurtigasi@gmail.com>
2025-03-10 18:23:18 -0300

    Diáogo
    iniciando os diálogos

--- Assets/Rafaela/Scripts/Dialogue.cs
+++ Assets/Rafaela/Scripts/Dialogue.cs
@@ -1,16 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Dialogue : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    public Sprite profile;
+    public string speechTxt;
+    public string actorName;
+
+    public LayerMask playerLayer;
+    public float radious;
+
+    //chamar o método de dialoguecontrol
+
+    private DialogueControl dc;
+
+    private void Start()
+    {   //find qdo iniciar o jogo ele procura na cena o lugar que tem o dialoguecontrol
+        dc = FindObjectOfType<DialogueControl>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Interact();
+    }
+
+    public void Interact() 
+    { //interação do npc com o player, vou criar um colisor invisível no contorno do npc
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, radious, playerLayer); //vai criar um colisor invisível circular
+
+        if ( hit != null)
+        {
+            dc.Speech(profile, speechTxt, actorName); 
+        }
     }
 }
