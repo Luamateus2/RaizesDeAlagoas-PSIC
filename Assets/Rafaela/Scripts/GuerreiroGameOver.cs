f1ee9465bf7b94e47d78ec8342fe7c6b4a123d9f
Rafaela Urtiga <rafaurtigasi@gmail.com>
2025-04-24 18:48:24 -0300

    scritps prontos

---
+++ Assets/Rafaela/Scripts/GuerreiroGameOver.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GuerreiroGameOver : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if (collision.gameObject.name == "Player") 
+        {
+            float playerPosY = collision.transform.position.y;
+            float guerreiroPosY = transform.position.y;
+
+            // Jogador está acima do guerreiro o suficiente para derrotá-lo
+            if (playerPosY > guerreiroPosY + 0.5f)
+            {
+                Debug.Log("Guerreiro derrotado!");
+                Destroy(gameObject);
+            }
+            else
+            {
+                Debug.Log("Player derrotado!");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+}
