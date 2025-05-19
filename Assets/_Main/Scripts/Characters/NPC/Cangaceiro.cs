37f3814926c6626a66c58bc8e31e2bd722bc9a26
Rafaela Urtiga <rafaurtigasi@gmail.com>
2025-05-19 11:15:56 -0300

    NPCs
    todos os npcs foram colocados nos cenários

---
+++ Assets/_Main/Scripts/Characters/NPC/Cangaceiro.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Cangaceiro: MonoBehaviour {
+    public Transform pontoA;
+    public Transform pontoB;
+    public float velocidade = 2f;
+
+    private Vector3 alvoAtual;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    void Start() {
+        alvoAtual = pontoB.position;
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (animator != null) {
+            animator.Play("Cangaceiro");
+        }
+    }
+
+    void Update() {
+        alvoAtual.z = transform.position.z;
+        transform.position = Vector3.MoveTowards(transform.position, alvoAtual, velocidade * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, alvoAtual) < 0.1f) {
+            // Alterna entre ponto A e ponto B
+            alvoAtual = (alvoAtual.x == pontoA.position.x) ? pontoB.position : pontoA.position;
+
+
+
+            // Espelha o sprite horizontalmente
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+    }
+}
