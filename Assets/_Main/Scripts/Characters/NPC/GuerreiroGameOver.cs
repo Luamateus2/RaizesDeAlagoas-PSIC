46d9c1b542aa9d38d37d6a110bcf6f5b1d6ce581
Rafaela Urtiga <rafaurtigasi@gmail.com>
2025-05-24 16:01:27 -0300

    consertos nos npcs

--- Assets/_Main/Scripts/Characters/NPC/GuerreiroGameOver.cs
+++ Assets/_Main/Scripts/Characters/NPC/GuerreiroGameOver.cs
@@ -3,13 +3,9 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class GuerreiroGameOver : MonoBehaviour {
-    // Referência ao SpriteRenderer do Guerreiro
-    private SpriteRenderer spriteRenderer;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
-        // Pega o componente SpriteRenderer do Guerreiro
-        spriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
@@ -25,32 +21,11 @@ public class GuerreiroGameOver : MonoBehaviour {
             // Jogador está acima do guerreiro o suficiente para derrotá-lo
             if (playerPosY > guerreiroPosY + 0.5f) {
                 Debug.Log("Guerreiro derrotado!");
-                // Inicia o efeito de piscar
-                StartCoroutine(BlinkAndDestroy());
+                Destroy(gameObject);
             } else {
                 Debug.Log("Player derrotado!");
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
         }
-    }
-
-    // Coroutine para fazer o Guerreiro piscar duas vezes e sumir
-    private IEnumerator BlinkAndDestroy() {
-        // Número de piscadas (2 vezes)
-        int blinkCount = 2;
-        // Tempo de cada estado (visível ou invisível)
-        float blinkDuration = 0.2f;
-
-        for (int i = 0; i < blinkCount; i++) {
-            // Desativa o sprite (invisível)
-            spriteRenderer.enabled = false;
-            yield return new WaitForSeconds(blinkDuration);
-            // Ativa o sprite (visível)
-            spriteRenderer.enabled = true;
-            yield return new WaitForSeconds(blinkDuration);
-        }
-
-        // Destroi o objeto após as piscadas
-        Destroy(gameObject);
     }
 }
