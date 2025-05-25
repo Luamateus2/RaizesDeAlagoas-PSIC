97bf8b869f08f5a47b8b5af840466f71b85f1d40
Lua Mateus <104106208+Luamateus2@users.noreply.github.com>
2025-05-25 19:58:56 -0300

    atualizações da fase 3

--- Assets/_Main/Scripts/Characters/Player rafa/ResertarMilitarTrigger.cs
+++ Assets/_Main/Scripts/Characters/Player rafa/ResertarMilitarTrigger.cs
@@ -5,12 +5,8 @@ public class ResertarMilitarTrigger : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+               MilitarTrigger.ResetarContador();
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
